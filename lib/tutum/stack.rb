07c271c491b17bcbfe6b12f4a92b2264cba35ab4
Adrien Folie <folie.adrien@gmail.com>
2015-03-27 14:20:18 +0000

    remove module key surrounding stack classé

--- lib/tutum/stack.rb
+++ lib/tutum/stack.rb
@@ -1,31 +1,29 @@
-module Tutum
-  class Stack < Struct.new(:name, :file)
-    def exists?
-      system "tutum stack inspect #{name} > /dev/null"
-    end
+class Stack < Struct.new(:name, :file)
+  def exists?
+    system "tutum stack inspect #{name} > /dev/null"
+  end
 
-    def create
-      puts "Creating #{name}..."
-      sh "tutum stack create --file #{file} --name #{name}"
-      puts "Created!"
-    end
+  def create
+    puts "Creating #{name}..."
+    sh "tutum stack create --file #{file} --name #{name}"
+    puts "Created!"
+  end
 
-    def update
-      puts "Updating #{name}..."
-      sh "tutum stack update --file #{file} #{name}"
-      puts "Updated!"
-    end
+  def update
+    puts "Updating #{name}..."
+    sh "tutum stack update --file #{file} #{name}"
+    puts "Updated!"
+  end
 
-    def redeploy
-      puts "Deploying #{name}..."
-      sh "tutum stack redeploy #{name}"
-      puts "#{name} is up and ready!"
-    end
+  def redeploy
+    puts "Deploying #{name}..."
+    sh "tutum stack redeploy #{name}"
+    puts "#{name} is up and ready!"
+  end
 
-    private
+  private
 
-    def sh(cmd)
-      abort unless system(cmd)
-    end
+  def sh(cmd)
+    abort unless system(cmd)
   end
 end
